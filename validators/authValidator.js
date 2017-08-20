3465f85a64abcd156f355bedf7403204c99007ba
Igor Alekseenko <fastedx@gmail.com>
2017-08-20 11:36:31 +0300

    few fixes add console logging for requests

--- validators/authValidator.js
+++ validators/authValidator.js
@@ -1,5 +1,7 @@
 var User = require("../user").user;
 
 module.exports = (searchModel) => {
+
+    //additional auth validation logic here
     return User.find(searchModel);
 }
--- validators/loginValidator.js
+++ validators/loginValidator.js
@@ -1,5 +1,7 @@
 var User = require("../user").user;
 
 module.exports = (searchModel) => {
+
+    //additional login validation logic here
     return User.find(searchModel);
 }
